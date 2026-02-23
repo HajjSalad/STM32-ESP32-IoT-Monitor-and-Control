2692862725078b116ff45bca5ad23bf36c5c02c5
Hajj <abdirahman.hjj@gmail.com>
2026-02-22 20:27:32 -0800

    Add Doxygen

--- main/wifi.c
+++ main/wifi.c
@@ -36,26 +36,51 @@ static void wifi_event_handler(void *event_handler_arg, esp_event_base_t event_b
     }
 }
 
-void wifi_connection()
+void wifi_connect()
 {
-    // 1 - Wi-Fi/LwIP Init Phase
-    esp_netif_init();                    // TCP/IP initiation 					s1.1
-    esp_event_loop_create_default();     // event loop 			                s1.2
-    esp_netif_create_default_wifi_sta(); // WiFi station 	                    s1.3
+    /**
+     *    1. TCP/IP and Wi-Fi Stack Initialization
+     *       ie Wi-Fi/LwIP Init Phase
+    */
+
+    // s1.1: Create an LwIP core task and initialize LwIP-related work
+    esp_netif_init();  
+
+    // s1.2: Create a system Event task and initialize an application event's callback function
+    esp_event_loop_create_default();
+
+    // s1.3: Create default network interface instance (biding station or AP) - sta used in this case
+    esp_netif_create_default_wifi_sta();
+
+    // s1.4: Create the Wi-Fi driver task and initialize the Wi-Fi driver with default settings
     wifi_init_config_t wifi_initiation = WIFI_INIT_CONFIG_DEFAULT();
-    esp_wifi_init(&wifi_initiation); // 					                    s1.4
-    // 2 - Wi-Fi Configuration Phase
+    esp_wifi_init(&wifi_initiation);
+
+    /** 
+     *    2. Wi-Fi Configuration Phase
+    */
+
+    /** 
+     *  s2.1 Register the wifi_event_hanlder function with the event loop
+     *      ESP_EVENT_ANY_ID    - listens for any Wi-Fi event (connect, disconnect, start etc)
+     *      IP_EVENT_STA_GOT_IP - listens for IP event when DHCP assigns an IP address
+    */
     esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, wifi_event_handler, NULL);
     esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, wifi_event_handler, NULL);
+
     wifi_config_t wifi_configuration = {
         .sta = {
             .ssid = SSID,
             .password = PASS
-            }
-        };
+        }
+    };
+    // Set the credentials the Wi-Fi driver will use to connect
+    // Also writes credentials to NVS flash for persistence across reboots
     esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_configuration);
-    // 3 - Wi-Fi Start Phase
+
+    // s3.1: Starts the Wi-Fi driver - Triggers `WIFI_EVENT_STA_START` in the event handler
     esp_wifi_start();
-    // 4- Wi-Fi Connect Phase
+
+    // s4.1: Wi-Fi Connect Phase - Triggers the actual connection attempt using the credentials
     esp_wifi_connect();
 }
